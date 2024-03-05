b4bc87a70a0d49346bfd8a17525109e01aa19e66
trafalgarzzz <trafalgarz@outlook.com>
2024-03-05 19:10:55 +0800

    Bump project backend builder to setuptools with pyproject.toml
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- fluid/__init__.py
+++ fluid/__init__.py
@@ -13,7 +13,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.1"
+__version__ = "0.1.2"
 
 # import apis into sdk package
 
--- setup.py
+++ setup.py
@@ -14,52 +14,6 @@
 
 #  coding: utf-8
 
-from importlib.machinery import SourceFileLoader
-from setuptools import setup, find_packages  # noqa: H301
+from setuptools import setup
 
-NAME = "fluid"
-
-def load_version(filename):
-    loader = SourceFileLoader(filename, filename)
-    return loader.load_module().VERSION
-
-
-def load_requirements(filename):
-    with open(filename) as fd:
-        return fd.readlines()
-
-
-requirements = load_requirements("requirements.txt")
-test_requirements = load_requirements("test-requirements.txt")
-
-setup(
-    name=NAME,
-    description="Fluid Python SDK",
-    version=load_version("fluid/version.py"),
-    author="Fluid Authors",
-    author_email="fluid.opensource.project@gmail.com",
-    url="https://github.com/fluid-cloudnative/fluid-client-python",
-    license="Apache License Version 2.0",
-    packages=find_packages(exclude=("tests",)),
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: Apache Software License'
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    tests_require=test_requirements,
-    install_requires=requirements,
-    python_requires=">=3.7",
-)
+setup()
